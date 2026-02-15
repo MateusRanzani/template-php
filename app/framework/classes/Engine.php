160d40b4d0ee67d14bf9a8b541a4737dfbf79a58
MateusRanzani <mateusranzani@hotmail.com>
2026-02-15 20:38:11 -0300

    update

---
+++ app/framework/classes/Engine.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\framework\classes;
+
+use Exception;
+
+class Engine
+{
+    public function render(string $view, array $data)
+    {
+        $view = dirname(__FILE__, 2) . "/resources/views/{$view}.php";
+        if (!file_exists($view)) {
+            throw new Exception("Template {$view} não encontrado");
+        }
+
+        ob_start();
+        extract($data);
+        require $view;
+        $content = ob_get_contents();
+        ob_clean();
+        return $content;
+    }
+}
